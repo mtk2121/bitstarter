5018bb7057170807ab1f51cb6f3900ad652fad4d
Ubuntu <ubuntu@ip-10-164-30-88.ec2.internal>
2013-07-17 08:20:56 +0000

    changed buf to buffer

--- web.js
+++ web.js
@@ -1,10 +1,9 @@
 var express = require('express');
-
+var buffer = buffer.write(fs.readFileSync(index.html));
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync(index.html)));
-});
+  response.send(buffer.toString(buffer);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
