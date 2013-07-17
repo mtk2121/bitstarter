ce7c4b9eb9d7968f15136bfaf4a990869bdfa0fa
Ubuntu <ubuntu@ip-10-164-30-88.ec2.internal>
2013-07-17 08:43:01 +0000

    corrected buffer

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-var buffer = buffer.write(fs.readFileSync(index.html));
+var fs = require ('fs');
+var buf = new Buffer(fs.readFileSync("index.html"));
+var content = buf.toString();
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString(buffer);
+  response.send(content);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
